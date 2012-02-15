7243b4172c8535b374e698ec3ffbc6b6cd481883
Olivier Aubert <olivier.aubert@liris.cnrs.fr>
2012-02-16 00:14:40 +0100

    Move test code into jquery.load() callback

--- src/test.js
+++ src/test.js
@@ -1,10 +1,12 @@
-(function() {
-     tr = window.tracemanager.init_trace("test");
-     tr.set_default_subject("oaubert");
-     tr.trace("StartTracing", { foo: "bar" });
-     for (var i = 0; i < 10; i++) {
-         tr.trace("Iteration", { index: i });
-     }
-     tr.trace("EndTracing");
-     console.log(tr.list_obsels());
- })();
+    $(window).load(function() {
+                       console.log("Starting test")
+                       tr = window.tracemanager.init_trace("test");
+                       tr.set_sync_mode('sync');
+                       tr.set_default_subject("oaubert");
+                       tr.trace("StartTracing", { foo: "bar" });
+                       for (var i = 0; i < 10; i++) {
+                           tr.trace("Iteration", { index: i });
+                       }
+                       tr.trace("EndTracing");
+                       console.log(tr.list_obsels());
+                   });
