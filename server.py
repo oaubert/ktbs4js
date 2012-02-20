87952d43503f20085ba187cbe7bffd19821022d6
Olivier Aubert <olivier.aubert@liris.cnrs.fr>
2012-02-20 11:45:38 +0100

    Implement /trace GET method

--- server.py
+++ server.py
@@ -2,9 +2,10 @@
 
 import os
 import json
+import bson
 import uuid
 from flask import Flask
-from flask import session, request, redirect, url_for
+from flask import session, request, redirect, url_for, jsonify, current_app
 import pymongo
 
 # PARAMETRES
@@ -16,6 +17,13 @@ connection = pymongo.Connection("localhost", 27017)
 db = connection[DB]
 
 app = Flask(__name__)
+
+class MongoEncoder(json.JSONEncoder):
+    def default(self, obj, **kwargs):
+        if isinstance(obj, bson.ObjectId):
+            return str(obj)
+        else:            
+            return json.JSONEncoder.default(obj, **kwargs)
 
 @app.route("/")
 def index():
@@ -43,7 +51,14 @@ def login():
         app.logger.debug("Logged in as " + session['userinfo']['id'])
     return redirect(url_for('index'))
 
-@app.route('/trace', methods= [ 'POST' ])
+def iter_obsels():
+    for o in db['trace'].find():
+        o['@id'] = o['_id']
+        del o['_id']
+        del o['_serverid']
+        yield o
+
+@app.route('/trace', methods= [ 'POST', 'GET' ])
 def trace():
     if request.method == 'POST':
         # Handle posting obsels to the trace
@@ -53,6 +68,18 @@ def trace():
             obsel['_serverid'] = session['userinfo']['id'];
             db['trace'].save(obsel)
         return "%d obsels stored" % len(obsels)
+    elif request.method == 'GET':
+        return current_app.response_class( json.dumps({ 
+                    "@context": [
+                        "http://liris.cnrs.fr/silex/2011/ktbs-jsonld-context",
+                        #{ "m": "http://localhost:8001/base1/model1#" }
+                        ],
+                    "@id": ".",
+                    "hasObselList": "",
+                    'obsels': list(iter_obsels()) },
+                                                      indent=None if request.is_xhr else 2,
+                                                      cls=MongoEncoder), 
+                                           mimetype='application/json')
 
 @app.route('/logout')
 def logout():
