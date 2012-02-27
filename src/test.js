f78efac543093c497023e12de5cec309e80e31eb
Olivier Aubert <olivier.aubert@liris.cnrs.fr>
2012-02-27 12:18:20 +0100

    Test binding even collection to button in test.html

--- src/test.js
+++ src/test.js
@@ -1,12 +1,15 @@
-    $(window).load(function() {
-                       console.log("Starting test")
-                       tr = window.tracemanager.init_trace("test");
-                       tr.set_sync_mode('sync');
-                       tr.set_default_subject("oaubert");
-                       tr.trace("StartTracing", { foo: "bar" });
-                       for (var i = 0; i < 10; i++) {
-                           tr.trace("Iteration", { index: i });
-                       }
-                       tr.trace("EndTracing");
-                       console.log(tr.list_obsels());
-                   });
+$(window).load(function() {
+                   console.log("Starting test")
+                   tr = window.tracemanager.init_trace("test");
+                   tr.set_sync_mode('delayed');
+                   tr.set_default_subject("oaubert");
+                   tr.trace("StartTracing", { foo: "bar" });
+                   for (var i = 0; i < 10; i++) {
+                       tr.trace("Iteration", { index: i });
+                   }
+                   console.log(tr.list_obsels());
+
+                   $('#b').bind('mouseover', function () { tr.trace('MouseOver', {widget: this.id}) } )
+                       .bind('mouseout', function () { tr.trace('MouseOut', {widget: this.id}) } )
+                       .bind('click', function () { tr.trace('Click', {widget: this.id}) } );
+               });
