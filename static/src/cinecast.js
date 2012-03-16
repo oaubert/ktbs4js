902b7430dc785e6874661c70bc566fa9bab3f868
Olivier Aubert <olivier.aubert@liris.cnrs.fr>
2012-03-16 15:09:46 +0100

    Add cinecast template

---
+++ static/src/cinecast.js
@@ -0,0 +1,13 @@
+$(window).load(function() {
+                   trace_cinecast = window.tracemanager.init_trace("festival", { uri: "http://traces.advene.org:5000/",
+                                                                                 requestmode: 'GET',
+                                                                                 syncmode: 'sync' });
+                   trace_cinecast.trace('SiteNavigate', { userid: userinfo ? userinfo.id : "anonymous",
+                                                          currenturl: document.URL,
+                                                          oldurl: document.referrer });
+                   /*
+                   $('#b').bind('mouseover', function () { tr.trace('MouseOver', {widget: this.id}) } )
+                       .bind('mouseout', function () { tr.trace('MouseOut', {widget: this.id}) } )
+                       .bind('click', function () { tr.trace('Click', {widget: this.id}) } );
+                    */
+               });
