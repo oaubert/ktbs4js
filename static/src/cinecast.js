70c127a190fe15f63b3d8d4aaf9052c896abf5cb
Olivier Aubert <olivier.aubert@liris.cnrs.fr>
2012-04-23 14:15:23 +0200

    Remove accents to prevent encoding issues

--- static/src/cinecast.js
+++ static/src/cinecast.js
@@ -6,10 +6,10 @@ $(window).load(function() {
                    trace_cinecast.trace('SiteNavigate', { currenturl: document.URL,
                                                           oldurl: document.referrer });
 
-                   /* Autres sondes à implémenter:
+                   /* Autres sondes à implementer:
                     * - UserLogin(userid, currenturl);
                     * - DoSearch(userid, currenturl, expression)
-                    * - (AnnotationCreate(userid, currenturl, annotationid) -> dérivable du mining des données)
+                    * - (AnnotationCreate(userid, currenturl, annotationid) -> dérivable du mining des donnees)
                     * - CommentCreate(userid, currenturl, annotationid, commenturl)
                     * - AnnotationShare(userid, currenturl, annotationid, service)
                     */
