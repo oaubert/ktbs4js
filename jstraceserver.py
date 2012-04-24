770e3009302614e5fd2f771e14be4f2d8943419a
Olivier Aubert <olivier.aubert@liris.cnrs.fr>
2012-04-24 16:31:36 +0200

    Specify LGPLv3 license

--- jstraceserver.py
+++ jstraceserver.py
@@ -1,5 +1,22 @@
 #! /usr/bin/python
 
+#
+# This file is part of ktbs4js.
+#
+# ktbs4js is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# ktbs4js is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with ktbs4js.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 import os
 import json
 import bson
--- static/src/tracemanager.js
+++ static/src/tracemanager.js
@@ -1,5 +1,21 @@
 /*
  * Modelled Trace API
+ *
+ * This file is part of ktbs4js.
+ *
+ * ktbs4js is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * ktbs4js is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with ktbs4js.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 /* FIXME: properly use require.js feature. This will do for debugging in the meantime */
 window.tracemanager = (function($) {
