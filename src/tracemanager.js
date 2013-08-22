9b25b02e6963b4afd6f21da72ae321870b760335
Olivier Aubert <contact@olivieraubert.net>
2013-08-23 01:10:10 +0200

    Add jsdoc3 documentation

--- src/tracemanager.js
+++ src/tracemanager.js
@@ -26,21 +26,38 @@
         window.tracemanager = factory(jQuery);
     }
 }(function ($) {
-    // If there are more than MAX_FAILURE_COUNT synchronisation
-    // failures, then disable synchronisation
-    MAX_FAILURE_COUNT = 20;
+    /**
+     * @constant
+     * @desc If there are more than MAX_FAILURE_COUNT synchronisation failures, then disable synchronisation
+     */
+    var MAX_FAILURE_COUNT = 20;
 
-    // If there are more than MAX_BUFFER_SIZE obsels in the buffer,
-    // then "compress" them as a single "ktbsFullBuffer"
-    MAX_BUFFER_SIZE = 500;
+    /**
+     * @constant
+     * @desc If there are more than MAX_BUFFER_SIZE obsels in the buffer, then "compress" them as a single "ktbsFullBuffer"
+     */
+    var MAX_BUFFER_SIZE = 500;
 
-    var logmsg = function() {
+    /** Log a message
+     */
+    function logmsg() {
         if (window.console) {
             window.console.log.apply(console, [ "ktbs4js" ].concat([].slice.call(arguments)));
         }
     };
 
-    var BufferedService = function(url, mode, format, handshake) {
+    /**
+     * @name BufferedService
+     * @class
+     * @constructor
+     * @param {string} url: the Trace url
+     * @param {string} mode: the HTTP method to use ("GET" or "POST")
+     * @param {string} format: the serialization format ("json", "json-compact", "turtle")
+     * @param {boolean} handshake: if true, send an initial handshake to the server
+     *
+     * @desc It takes care of enqueuing obsels and sending them possibly in batches to the server.
+     */
+    function BufferedService(url, mode, format, handshake) {
         this.url = url;
         this.buffer = [];
         this.isReady = !handshake;
@@ -60,17 +77,17 @@
         /* Flush buffer every timeOut ms if the sync_mode is delayed */
         this.timeOut = 2000;
     };
-    BufferedService.prototype = {
-        /*
-         *  Buffered service for traces
-         */
+
+    BufferedService.prototype = /** @lends BufferedService.prototype */ {
         // url: "",
         // buffer: [],
         // isReady: false,
         // timer: null,
         // failureCount: 0,
 
-        /* Flush buffer */
+        /**
+         * Flush buffer
+         */
         flush: function() {
             // FIXME: add mutex on this.buffer
             if (! this.isReady)
@@ -140,9 +157,13 @@
             }
         },
 
-        /* Sync mode: delayed, sync (immediate sync), none (no
-         * synchronisation with server, the trace has to be explicitly saved
-         * if needed */
+        /**
+         * Set sync mode: "delayed", "sync" (immediate sync), "none"
+         * (no synchronisation with server, the trace has to be
+         * explicitly saved if needed).
+         * @param {string} mode: "delayed", "sync" or "none"
+         * @param {string} default_subject: Default subject id
+         */
         set_sync_mode: function(mode, default_subject) {
             this.sync_mode = mode;
             if (! this.isReady && mode !== "none")
@@ -154,7 +175,10 @@
             }
         },
 
-        /* Enqueue an obsel */
+        /**
+         * Enqueue an obsel 
+         * @param {Obsel} obsel: the obsel to enqueue
+         **/
         enqueue: function(obsel) {
             if (this.buffer.length > MAX_BUFFER_SIZE)
             {
@@ -170,6 +194,9 @@
             }
         },
 
+        /**
+         * Start the timer for delayed sync
+         */
         start_timer: function() {
             var self = this;
             if (this.timer === null) {
@@ -179,6 +206,9 @@
             }
         },
 
+        /**
+         * Stop the timer for delayed sync
+         */
         stop_timer: function() {
             if (this.timer !== null) {
                 window.clearInterval(this.timer);
@@ -186,8 +216,9 @@
             }
         },
 
-        /*
+        /**
          * Initialize the sync service
+         * @param {string} default_subject: the default subject
          */
         init: function(default_subject) {
             var self = this;
@@ -223,7 +254,17 @@
         }
     };
 
-    var Trace = function(uri, mode, requestmode, format, handshake) {
+    /**
+     * @name Trace
+     * @class
+     * @constructor
+     * @param {string} uri: the Trace uri
+     * @param {string} mode: the access mode ("r", "w" or "rw")
+     * @param {string} requestmode: the HTTP method used for synchronization ("GET" or "POST")
+     * @param {string} format: the serialization format (see BufferedService)
+     * @param {boolean} handshake: see BufferedService
+     */
+    function Trace(uri, mode, requestmode, format, handshake) {
         /* FIXME: We could/should use a sorted list such as
            http://closure-library.googlecode.com/svn/docs/class_goog_structs_AvlTree.html
            to speed up queries based on time */
@@ -257,39 +298,43 @@
         }
     };
 
-    Trace.prototype = {
+    Trace.prototype = /** @lends Trace.prototype */ {
         /* FIXME: We could/should use a sorted list such as
            http://closure-library.googlecode.com/svn/docs/class_goog_structs_AvlTree.html
            to speed up queries based on time */
+        /** List of obsels */
         obsels: [],
-        /* Trace URI */
+        /** Trace URI */
         uri: "",
-        /* mode defined the intended usage mode of the trace. It can be either
+        /** mode defines the intended usage mode of the trace. It can be either
            "r" : read-only - we will only read existing obsels
            "w" : write-only - we will only create obsels to be stored on a server
            "rw" : read-write - we will both read and write obsels.
         */
         mode: "rw",
+        /** Default subject */
         default_subject: "",
-        /* baseuri is used as the base URI to resolve relative
+        /** baseuri is used as the base URI to resolve relative
          * attribute-type names in obsels. Strictly speaking, this
          * should rather be expressed as a reference to model, or
          * more generically, as a qname/URI dict */
         baseuri: "",
-        /* Mapping of obsel type or property name to a compact
+        /** Mapping of obsel type or property name to a compact
          * representation (shorthands).
          */
         shorthands: null,
         syncservice: null,
 
-        /* Define the trace URI */
+        /** Define the trace URI */
         set_uri: function(uri) {
             this.uri = uri;
         },
+        /** Get the trace URI */
         get_uri: function() {
             return this.uri;
         },
 
+        /** Get the trace id */
         get_id: function() {
             var i = this.uri.split("/").reverse();
             if (i[0] !== "")
@@ -298,11 +343,12 @@
                 return i[1];
         },
 
-        // FIXME: to implement
+        /** Indicates wether the trace can be written to */
         get_readonly: function() {
-            return true;
+            return (this.mode.indexOf("w") === -1);
         },
 
+        /** Remove the trace from the server */
         remove: function() {
             $.ajax({ url: this.uri,
                      type: 'DELETE',
@@ -314,26 +360,23 @@
             return true;
         },
 
-        // FIXME: implement properly
-        get_label: function() {
-            return this.id;
-        },
-        
-        /* Sync mode: delayed, sync (immediate sync), none (no
-         * synchronisation with server, the trace has to be explicitly saved
-         * if needed */
+        /**
+         * Set sync mode. See {@link BufferedService.set_sync_mode} for details.
+         * @param {string} mode: "delayed", "sync" or "none"
+         */
         set_sync_mode: function(mode) {
             if (this.syncservice !== null) {
                 this.syncservice.set_sync_mode(mode, this.default_subject);
             }
         },
 
-        /* Load obsels from the Trace url
+        /** Load obsels from the Trace url
          * options is an object that contains optional parameters:
          *  - page: the page number
          *  - pagesize: the page size
          *  - from: the minimum timestamp
          *  - to: the maximum timestamp
+         * @param {object} options: optional parameters
          */
         load_obsels: function(options) {
             var self = this;
@@ -372,12 +415,17 @@
                    });
         },
 
+        /** Force trace refresh */
         force_state_refresh: function() {
             this.load_obsels();
         },
 
-        /*
+        /**
          * Return a list of the obsels of this trace matching the parameters
+         * @param {integer} _begin: the minimum begin time
+         * @param {integer} _end: the maximum end time
+         * @param {boolean} _reverse: return obsels in reverse chronological order
+         * @return {list} a list of {@links Obsel}
          */
         list_obsels: function(_begin, _end, _reverse) {
             var res;
@@ -408,11 +456,11 @@
                 res = this.obsels;
             }
             return res;
-
         },
 
-        /*
+        /**
          * Return the obsel of this trace identified by the URI, or undefined
+         * @return {Obsel} an obsel
          */
         get_obsel: function(id) {
             for (var i = 0; i < this.obsels.length; i++) {
@@ -424,6 +472,9 @@
             return undefined;
         },
 
+        /** Set the default subject
+         * @param {string} subject
+         */
         set_default_subject: function(subject) {
             // FIXME: if we call this method after the sync_service
             // init method, then the default_subject will not be
@@ -431,12 +482,22 @@
             this.default_subject = subject;
         },
 
+        /** Get the default subject
+         * @return {string} the default subject
+         */
         get_default_subject: function() {
             return this.default_subject;
         },
 
         /* (ident: id, type:ObselType, begin:int, end:int?, subject:str?, attributes:[AttributeType=>any]?) */
-        /* Create a new obsel and add it to the trace */
+        /** Create a new obsel and add it to the trace 
+         * @param {string} ident: the obsel id
+         * @param {string} type: the obsel type
+         * @param {integer} begin: the obsel begin time
+         * @param {integer} end: the obsel end time
+         * @param {object} _attributes: the obsel attributes
+         * @return {Obsel} the created obsel
+         */
         create_obsel: function(ident, type, begin, end, subject, _attributes) {
             var o = new Obsel(ident, type, begin, end, subject);
             if (typeof _attributes !== 'undefined') {
@@ -447,11 +508,12 @@
             if (this.syncservice !== null)
                 this.syncservice.enqueue(o);
             $(this).trigger('updated');
+            return o;
         },
 
         /* Helper methods */
 
-        /*
+        /**
          * Parse a JSON-LD trace representation and add resulting
          * obsels to the trace obsels.
          */
@@ -470,10 +532,16 @@
             }
         },
 
-        /* Create a new obsel with the given attributes */
+        /** Create a new obsel with the given attributes 
+         * @param {string} type: the obsel type
+         * @param {object} _attributes: the obsel attributes
+         * @param {integer} [_begin]: the obsel begin time (current time if omitted)
+         * @param {integer} [_end]: the obsel end time (same as begin if omitted)
+         * @param {string} [_subject]: the obsel subject (trace default subject if omitted)
+         */
         trace: function(type, _attributes, _begin, _end, _subject) {
-            var t = (new Date()).getTime();
             if (typeof begin === 'undefined') {
+                var t = (new Date()).getTime();
                 _begin = t;
             }
             if (typeof end === 'undefined') {
@@ -489,7 +557,18 @@
         }
     };
 
-    var Obsel = function(ident, type, begin, end, subject, attributes) {
+    /**
+     * @name Obsel
+     * @class
+     * @constructor
+     * @param {string} ident: the obsel id
+     * @param {string} type: the obsel type
+     * @param {integer} begin: the obsel begin time
+     * @param {integer} end: the obsel end time
+     * @param {string} subject: the obsel subject
+     * @param {object} attributes: the obsel attributes
+     */
+    function Obsel(ident, type, begin, end, subject, attributes) {
         this.trace = undefined;
         this.uri = "";
         this.id = ident || "";
@@ -502,7 +581,7 @@
         /* Dictionary indexed by ObselType URIs */
         this.attributes = {};
     };
-    Obsel.prototype = {
+    Obsel.prototype = /** @lends Obsel.prototype */ {
         /* The following attributes are here for documentation
          * purposes. They MUST be defined in the constructor
          * function. */
@@ -514,7 +593,9 @@
         /* Dictionary indexed by ObselType URIs */
         attributes: {},
 
-        /* Method definitions */
+        /** Get the Obsel URI
+         * @return {string} the URI
+         */
         get_uri: function() {
             if (this.uri)
                 return this.uri;
@@ -524,10 +605,14 @@
                 return "";
         },
 
+        /** Get the Obsel id
+         * @return {string} the id
+         */
         get_id: function() {
             return this.id;
         },
 
+        /** Force reload */
         force_state_refresh: function() {
             $.ajax({ url: this.uri,
                      type: 'GET',
@@ -548,6 +633,9 @@
             return true;
         },
 
+        /** Delete the obsel from the trace 
+         * @throws Will throw an exception if it could not be removed.
+         */
         remove: function() {
             $.ajax({ url: this.uri,
                      type: 'DELETE',
@@ -563,7 +651,7 @@
         get_label: function() {
             return this.id;
         },
-        
+
         get_trace: function() {
             return this.trace;
         },
@@ -581,6 +669,8 @@
             return this.subject;
         },
 
+        /** List attribute types 
+         */
         list_attribute_types: function() {
             var result = [];
             for (var prop in this.attributes) {
@@ -604,7 +694,7 @@
         list_relating_obsels: function (rt) {
             /* FIXME: not implemented yet */
         },
-        /*
+        /**
          * Return the value of the given attribute type for this obsel
          */
         get_attribute_value: function(at) {
@@ -619,6 +709,7 @@
 
         /* obsel modification (trace amendment) */
 
+        /** Set the value of an attribute */
         set_attribute_value: function(at, value) {
             if (typeof at === "string")
                 /* It is a URI */
@@ -628,6 +719,7 @@
                 this.attributes[at.uri] = value;
         },
 
+        /** Delete an attribute */
         del_attribute_value: function(at) {
             if (typeof at === "string")
                 /* It is a URI */
@@ -637,15 +729,17 @@
                 delete this.attributes[at.uri];
         },
 
+        /** Add a related obsel */
         add_related_obsel: function(rt, value) {
             /* FIXME: not implemented yet */
         },
 
+        /** Delete a related obsel */
         del_related_obsel: function(rt, value) {
             /* FIXME: not implemented yet */
         },
 
-        /*
+        /**
          * Return a JSON representation of the obsel
          */
         toJSON: function() {
@@ -663,11 +757,11 @@
             return r;
         },
 
-        /*
-         * Initialize the Obsel attributes from the given JSON-LD representation
-         */
         PRIVATE_JSON_PROPERTIES: [ '@id', '@type', 'subject', 'begin', 'end' ],
-
+        /**
+         * Initialize the Obsel attributes from the given JSON-LD representation
+         * @param {object} j: a JSON-LD serialization
+         */
         fromJSON: function(j) {
             this.id = j['@id'];
             this.type = j['@type'];
@@ -684,7 +778,7 @@
             return this;
         },
 
-        /*
+        /**
          * Return a compact JSON representation of the obsel.
          * Use predefined + custom shorthands for types/properties
          */
@@ -715,11 +809,12 @@
             return r;
         },
 
+        /** Return a JSON string serialization */
         toJSONstring: function() {
             return JSON.stringify(this.toJSON());
         },
 
-        /*
+        /**
          * Return a Turtle representation of the obsel,
          * assuming that : is bound to the KTBS namespace,
          * and that m: is bound to the model namespace.
@@ -742,26 +837,34 @@
 
     };
 
-    var TraceManager = function() {
+    /**
+     * @name TraceManager
+     * @class
+     * @constructor
+     * @desc It manages a set of traces.
+     */
+    function TraceManager() {
         this.traces = {};
     };
-    TraceManager.prototype = {
+    TraceManager.prototype = /** @lends TraceManager.prototype */ {
         traces: [],
-        /*
+        /**
          * Return the trace with id name
          * If it was not registered, return undefined.
+         * @param {string} name
          */
         get_trace: function(name) {
             return this.traces[name];
         },
 
-        /*
+        /**
          * Explicitly create and initialize a new trace with the given name.
          * The optional uri parameter allows to initialize the trace URI.
+         * If another trace exists with the same name, then it is replaced by a new one.
          *
-         * If another existed with the same name before, then it is replaced by a new one.
-         *
-         * See the Trace documentation for the definition of parameters.
+         * See the {@link Trace} documentation for the definition of parameters.
+         * @param {string} name: the trace name
+         * @param {object} params: the trace parameters
          */
         init_trace: function(name, params)
         {
@@ -780,6 +883,9 @@
         }
     };
 
+    /** @name tracemanager
+     * @desc The {@link TraceManager} instance (singleton)
+     */
     var tracemanager  = new TraceManager();
     return tracemanager;
 }));
