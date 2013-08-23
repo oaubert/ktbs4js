f1fe0d0dc5fdbe23f25e8bcc9b3a362bdb47a1f5
Olivier Aubert <contact@olivieraubert.net>
2013-08-23 13:22:04 +0200

    Update example file

--- src/example.js
+++ src/example.js
@@ -1,37 +1,24 @@
 // -*- coding: utf-8; -*-
+/* Example ktbs4js usage */
 require(['jquery', 'tracemanager'], function($, tracemanager) {
-            $(window).load(function() {
-                   // Initialisation of default_subject is
-                   // VodKaster-specific: other partners should modify
-                   // the default_subject initialisation to get user-id
-                   trace_cinecast = tracemanager.init_trace("festival", { url: "http://traces.advene.org:5000/",
-                                                                          requestmode: 'GET',
-                                                                          syncmode: 'sync',
-                                                                          default_subject: typeof(userInfo) != 'undefined' ? userInfo.id : 'anonymous' });
-                   trace_cinecast.trace('SiteNavigate', { currenturl: document.URL,
-                                                          oldurl: document.referrer });
+    $(window).load(function() {
+        // Initialisation of default_subject is site-specific
+        my_trace = tracemanager.init_trace("base", { url: "http://server.address/base/",
+                                                     requestmode: 'POST',
+                                                     syncmode: 'sync',
+                                                     default_subject: typeof(userInfo) != 'undefined' ? userInfo.id : 'anonymous' });
+        my_trace.trace('SiteNavigate', { currenturl: document.URL,
+                                         oldurl: document.referrer });
+        
+        /* 
+           As the userid has been specified as default_subject for the
+           trace, it will be added to every collected obsel, there is
+           no need to specify it on each call to my_trace.trace.
+        */
 
-                   /* Autres sondes à implémenter:
-                    * - UserLogin(userid, currenturl);
-                    * - DoSearch(userid, currenturl, expression)
-                    * - (AnnotationCreate(userid, currenturl, annotationid) -> dérivable du mining des données)
-                    * - CommentCreate(userid, currenturl, annotationid, commenturl)
-                    * - AnnotationShare(userid, currenturl, annotationid, service)
-                    *
-                    * Note: cette notation traduit les types
-                    * d'observés ainsi que les attributs
-                    * associés pour les obsels qui seront obtenus.
-                    *
-                    * Au niveau de la collecte, comme le userid est
-                    * spécifié comme default_subject pour la trace, il
-                    * sera associé à chaque obsel. Il est donc inutile
-                    * de le spécifier lors de l'appel à trace().
-                    */
-                   /*
-                    * Exemple d'utilisation avec jquery:
-                    $('#b').bind('mouseover', function () { tr.trace('MouseOver', {widget: this.id}) } )
-                    .bind('mouseout', function () { tr.trace('MouseOut', {widget: this.id}) } )
-                    .bind('click', function () { tr.trace('Click', {widget: this.id}) } );
-                    */
-               });
-        });
+        /* Usage with  jquery. Timestamps will be automatically be generated. */
+        $('#b').bind('mouseover', function () { tr.trace('MouseOver', {widget: this.id}) } )
+            .bind('mouseout', function () { tr.trace('MouseOut', {widget: this.id}) } )
+            .bind('click', function () { tr.trace('Click', {widget: this.id}) } );
+    });
+});
